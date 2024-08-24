89fc53b419cf55c7704d97d02bb21395a81c1599
ShheNNa01 <151384675+ShheNNa01@users.noreply.github.com>
2024-08-24 10:23:24 -0500

    Clase 24/08/2024
    Se deja lo realizado en clase.

---
+++ Semestres/Semestre 3/NuevasTecnologias/ClaseFunciones/Funciones.py
@@ -0,0 +1,60 @@
+"""def miFuncion(mensaje):
+    mensaje = input("Ingrese por favor un mensaje")
+    #Incorporamos la variable al proceso 
+    print(f"El mensaje es{mensaje}")
+
+#Siempre se hace necesario llamar la funcion para el proceso de mostrar
+miFuncion(mensaje="mensaje")"""
+
+"""def miFuncion (n1,n2):
+    n1 = int (input ("Por favor ingrese el numero uno"))
+    n2 = int (input ("Por favor ingrese el numero dos"))
+    print ("El resultado es ", n1 + n2)
+miFuncion(n1="n1" , n2="n2")"""
+
+#lista = [1,"nombre",1,54]
+#listados = list (1,2,"nombre")
+#Esto es para mostrar en reversa
+#print(listados[::-1])
+#Para mostrar elementos especificos
+#print(listados[0:2])
+#Para agregar elementos en la lista
+#listados.append(44)
+"""print(listados)
+#Para insertar en una posicion mediante insert
+listados.insert(2[44,85])
+print(listados)
+#Eliminar un elemento en epecifico
+listados.remove(2)
+print(listados)
+#El pop sirve para eliminar desde el indice y en reversa usamos el -(MENOS)
+listados.pop(-2)
+print(listados)
+
+#El cliente insertara datos
+edad = int (input("Por favor ingrese su edad"))
+listados = list (1,2,"nombre")
+listados.append
+print(listados)"""
+
+#Procedemos a crear un diccionario
+"""diccionario={"nombre" : "sebastian",1:12,
+             "edad":15}
+ndiccionario = dict (diccionario)  
+print(ndiccionario)
+ndiccionario ["ciudad"] = "Tangamandapio"
+print(ndiccionario)"""
+
+def funciond():
+    ciudad = input ("Ingrese la ciudad: ")
+    listac = [ciudad]
+    print (listac)
+    
+"""for i in range(3):
+        funciond ()
+        print(f" Este es el contador {i}")"""
+i=0
+while i < 3 :
+    funciond()
+    i += 1
+    print(f"El contador es {i}")
